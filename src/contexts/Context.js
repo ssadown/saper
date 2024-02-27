b76ae783eb170269190c3ad8fac2a319a45619bc
ssadown <111707454+ssadown@users.noreply.github.com>
2024-02-27 22:44:24 +0500

    Updates

--- src/contexts/Context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const loginContext = createContext()
-export const currentUserContext = createContext()
-export const leaderDashboardContext = createContext()
-export const gameSettingsContext = createContext()
